a05e9e5a9b3fc0e08bd2577ecd9cdc0bdbbb7746
Prashant Chakraborty <prashantchakraborty@Mac.attlocal.net>
2025-03-29 22:44:04 -0500

    Angular-Training-1

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+// set standalone to true if angular version is below 19
+// else do not mention it, since it is not needed then.
+// standalone property is true also means that this component
+// is a standalone component. Else, it is a module based component.
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+})
+
+export class HeaderComponent {}
